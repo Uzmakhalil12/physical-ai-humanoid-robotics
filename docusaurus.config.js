9261d81449d8d46db0d83c704d433aebaf847a40
Uzmakhalil12 <147942579+Uzmakhalil12@users.noreply.github.com>
2025-12-17 00:53:33 +0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -134,7 +134,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright uzmakhalil © ${new Date().getFullYear()} physical-ai,Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
